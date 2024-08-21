cdf563ead1a3ef395de5b58156586f25addc6835
Husky-4559 <110746587+Husky-4559@users.noreply.github.com>
2024-08-21 16:41:40 -0700

    code updated, ready for submission

--- app.js
+++ app.js
@@ -1,52 +1,57 @@
-// Part One
+/** Type of vehicle */
+
 class Vehicle {
-  constructor (make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-  }
-  honk() {
-    return "Beep.";
-  }
-  toString() {
-    return `The vehicle is a ${make} ${model} from ${this.year}.`;
-  }
+	constructor(make, model, year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	honk() {
+		return "Beep.";
+	}
+
+	toString() {
+		return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+	}
 }
 
-//Part Two
+/** Cars are a type of vehicle with 4 wheels. */
+
 class Car extends Vehicle {
-  constructor (make, model, year) {
-    super(make, model, year);
-    this.numWheels = 4;
-  }
+	constructor(make, model, year) {
+		super(make, model, year);
+		this.numWheels = 4;
+	}
 }
 
-//Part Three
+/** Cars are a type of vehicle with 2 wheels. */
+
 class Motorcycle extends Vehicle {
-  constructor(make, model, year) {
-    super(make, model, year);
-    this.numWheels = 2;
-  }
-  revEngine() {
-    return "VROOM!!!!";
-  }
-}
+	constructor(make, model, year) {
+		super(make, model, year);
+		this.numWheels = 2;
+	}
 
-//Part Four
+	revEngine() {
+		return "VROOM!!!";
+	}
+}
 
 class Garage {
-  constructor(capacity){
-    this.vehicles = [];
-    this.capacity = capacity;
-  }
-  add (newVehicle) {
-    if (!(newVehicle instanceof Vehicle)) {
-      return "Only vehicles are allowed in here!"
-    }
-    if (this.vehicles.length <= this.capacity) {
-      return "Sorry, we're full.";
-    }
-    this.vehicles.push(newVehicle);
-    return "Vehicle added!";
-  }
+	constructor(capacity) {
+		this.vehicles = [];
+		this.capacity = capacity;
+	}
+
+	add(newVehicle) {
+		if (!(newVehicle instanceof Vehicle)) {
+			return "Only vehicles are allowed in here!";
+		}
+		if (this.vehicles.length >= this.capacity) {
+			return "Sorry, we're full.";
+		}
+		this.vehicles.push(newVehicle);
+		return "Vehicle added!";
+	}
 }
